fab72c00d252105240897138de042596984d5d3f
Abdullah A.A <abdullahsurnamea786@gmail.com>
2025-05-13 23:05:15 +0500

    adding icon in hero and changing meta data

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Hero: React.FC = () => {
   return (
     <section
-      className="text-white h-[110vh] w-full overflow-hidden bg-cover bg-top flex items-center"
+      className="text-white h-[110vh] w-full overflow-hidden bg-cover bg-top flex items-center "
       style={{
         backgroundImage: "url('./Hero.png')",
       }}
@@ -18,12 +18,16 @@ const Hero: React.FC = () => {
             <br className="hidden md:block" />
             the latest regulations?
           </h2>
-          <p className="text-base sm:text-lg mb-2 text-blue-200 font-medium">
-            AI-Powered Frontline Governance
-          </p>
-          <p className="text-sm mb-8 text-blue-100">
-            an operational compliance solution
-          </p>
+          <div className="flex gap-2 items-center justify-start my-8 sm:ml-[-30px]">
+            <img
+              src="./white-logo.png"
+              alt="Logo"
+              className="h-[50px] w-[100px] sm:w-40 sm:h-20"
+            />
+            <p className="text-base sm:text-lg mb-2 text-white font-medium">
+              AI-Powered Frontline Governance
+            </p>
+          </div>
 
           <button className="bg-blue-600 text-white max-w-[200px] font-medium px-6 py-3 rounded-md transition-all duration-300 transform hover:scale-105">
             Request a Demo
