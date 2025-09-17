374cb1b329018bebdbcc40c4f9608d545e4d9d4b
Hassan Ali <hassanraoofficial@gmail.com>
2025-09-17 10:12:13 +0500

    health-care unclickable hero button fixed

--- src/Pages/HealthCare/Components/Hero.tsx
+++ src/Pages/HealthCare/Components/Hero.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Hero: React.FC = () => {
   const [isWideScreen, setIsWideScreen] = useState(false);
@@ -18,7 +18,11 @@ const Hero: React.FC = () => {
     <section
       className="relative h-full lg:min-h-[120vh] w-full lg:overflow-hidden bg-cover bg-bottom-center bg-top flex items-center"
       style={{
-        backgroundImage: `url('./${isWideScreen ? 'healthcare-hero-bg.png' : 'healthcare-hero-mobile-bg.png'}')`,
+        backgroundImage: `url('./${
+          isWideScreen
+            ? "healthcare-hero-bg.png"
+            : "healthcare-hero-mobile-bg.png"
+        }')`,
       }}
     >
       <div className="absolute top-0 left-0 w-full h-full bg-blue-900 md:bg-transparent bg-opacity-50 z-10"></div>
@@ -29,7 +33,11 @@ const Hero: React.FC = () => {
             data-aos="fade-right"
           >
             <div className="max-w-[500px]">
-            <img src="./desktop-plus-mobile.png" className="hidden lg:block w-full h-full object-cover" alt="" />
+              <img
+                src="./desktop-plus-mobile.png"
+                className="hidden lg:block w-full h-full object-cover"
+                alt=""
+              />
             </div>
             <h2 className="text-2xl sm:text-3xl md:text-4xl font-normal mb-2 leading-tight text-white">
               A critical tool to support {isWideScreen ? <br /> : " "}
@@ -40,13 +48,21 @@ const Hero: React.FC = () => {
               are managed and distributed for frontline workers.
             </p>
 
-            <div className="flex md:justify-start justify-center">
-              <Link to="/contact" className="bg-blue-700 text-white text-medium max-w-[275px] text-[15px] font-medium px-4 py-2 rounded-md transition-all duration-300 transform hover:scale-105">
+            <div className="flex md:justify-start justify-center mb-10">
+              <Link
+                to="/contact"
+                className="bg-blue-700 text-white text-medium max-w-[275px] text-[15px] font-medium px-4 py-2 rounded-md transition-all duration-300 transform hover:scale-105"
+              >
                 Try MyStaff app - See How It Works
               </Link>
             </div>
-            <div className="absolute -bottom-[140px] left-1/2 transform -translate-x-1/2 w-full max-w-[350px] lg:max-w-[600px]">
-            <img src="./desktop-plus-mobile.png" className="lg:hidden mt-20 w-full h-full object-cover" alt="" /></div>
+            <div className="absolute -bottom-[115px] left-1/2 transform -translate-x-1/2 w-full max-w-[350px] lg:max-w-[600px]">
+              <img
+                src="./desktop-plus-mobile.png"
+                className="lg:hidden w-full h-full object-cover"
+                alt=""
+              />
+            </div>
           </div>
         </div>
       </div>
