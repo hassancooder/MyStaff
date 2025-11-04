d346de429384f9898b7e63ae152937695871bf3b
Hassan Ali <hassanraoofficial@gmail.com>
2025-11-04 13:12:48 +0500

    Update images and lnk to app pages footer

--- src/Pages/AccessibilityStatement/Components/Hero.tsx
+++ src/Pages/AccessibilityStatement/Components/Hero.tsx
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const Hero: React.FC = () => {
+  const [isWideScreen, setIsWideScreen] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsWideScreen(window.innerWidth > 700);
+    };
+
+    handleResize(); // check on first load
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
   return (
     <section
-      className={`min-h-[100vh] w-full overflow-hidden bg-cover bg-top flex items-center`}
+      className={`min-h-[100vh] w-full overflow-hidden bg-cover bg-center flex items-center`}
       style={{
-        backgroundImage: "url('./accessibility-statement-hero-bg.webp')",
+        backgroundImage: `url('./as-hero-bg-${
+          isWideScreen ? "desktop" : "mobile"
+        }.webp')`,
       }}
     >
       <div className="container mx-auto px-4 sm:px-6 lg:px-20 py-16">
@@ -14,19 +28,12 @@ const Hero: React.FC = () => {
             className="flex-1 max-w-2xl text-center md:text-left"
             data-aos="fade-right"
           >
-            <h2 className="text-2xl sm:text-3xl md:text-4xl font-normal mb-2 leading-tight text-white">
-              <p className="font-semibold">Accessibility <br/>Statement</p> 
+            <h2 className="text-3xl md:text-4xl font-normal mb-2 leading-tight text-white">
+              <p className="font-semibold">
+                Accessibility <br />
+                Statement
+              </p>
             </h2>
-          </div>
-          <div
-            className="flex-1 max-w-2xl"
-            data-aos="fade-left"
-          >
-            <img 
-              src="./as-hero-fimage.webp" 
-              alt="Accessibility Statement Front Image" 
-              className="w-full h-auto object-contain" 
-            />
           </div>
         </div>
       </div>
--- src/Pages/Finance/Components/Footer.tsx
+++ src/Pages/Finance/Components/Footer.tsx
@@ -1,16 +1,26 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Footer: React.FC = () => {
   return (
-    <footer id="contact" className="bg-black text-white py-6 h-auto w-full">
-      <div className="container mx-auto px-4">
-        <div className="text-center">
-          <h2 className="text-md font-bold sm:text-lg">
-            © 2025 Diligram. All rights reserved.
-          </h2>
+    <>
+      <footer className="bg-black py-5 h-auto w-full">
+        <div className="container mx-auto text-center px-4">
+            <Link to="/accessibility-statement" className="text-md hover:underline text-center text-white font-medium">
+              Accessibility Statement
+            </Link>
         </div>
-      </div>
-    </footer>
+      </footer>
+      <footer id="contact" className="bg-black border-t-2 border-gray-400 text-white pt-4 pb-6 h-auto w-full">
+        <div className="container mx-auto px-4">
+          <div className="text-center">
+            <h2 className="text-md font-bold sm:text-lg">
+              © 2025 Diligram. All rights reserved.
+            </h2>
+          </div>
+        </div>
+      </footer>
+    </>
   );
 };
 
